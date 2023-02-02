4d8eea270b1e3e3bc8d37b921564e6fc68143af3
Smoke5643 <smoke5643@gmail.com>
2023-02-01 20:39:32 -0500

    set up User and Thought models

---
+++ models/Thought.js
@@ -0,0 +1,39 @@
+const { Schema, model } = require('mongoose');
+const reactionSchema = require('./Reaction');
+const userSchema = require('./User');
+
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      min_length: 1,
+      max_length: 280,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      // get: (timestamp) => (timestamp),
+    },
+    username: {
+      type: Schema.Types.String,
+      ref: 'user',
+    },
+    reactions: [reactionSchema],
+  },
+  {
+    toJSON: {
+      getters: true,
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+thoughtSchema.virtual("reactionCount").get(function () {
+  return this.reactions.length;
+});
+
+const Thought = model('thought', thoughtSchema);
+
+module.exports = Thought;
